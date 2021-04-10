cef3ac646c3c8b3777e0a8f03f844a7b1574dab4
hoochi <themacdeluxe@gmail.com>
2021-04-10 16:54:32 +0100

    Delete et-colour/app directory

--- et-colour/app/js/dev/printable.js
+++
@@ -1,7 +0,0 @@
-(function(printable,$){
-   $(function() {
-      et.buildPrintableCharacters($('#colourControls'));
-      $('#tag').html(et.buildColour('^I#^7hoochi'));
-      $('[data-toggle="tooltip"]').tooltip();
-   });
-})(window.printable = window.printable || {}, jQuery);
